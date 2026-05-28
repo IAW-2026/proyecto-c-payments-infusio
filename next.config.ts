fc0c639119f4523ba03da14e6979d537537b9232
Lautaro <emalhao.142047@alu.uns.edu.ar>
2026-05-28 19:00:36 -0300

    fix hero actions

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   compress: true,
   images: {
-    formats: ["image/avif", "image/webp"],
+    formats: ["image/webp"],
   },
 };
 
